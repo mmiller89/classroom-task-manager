e3f1eb916cbe1e2b9c082645e4e3c044cb098fff
Michael <mchlmiller89@gmail.com>
2024-03-13 20:58:08 -0700

    Readying for Gitpages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,7 +78,7 @@ export class AppComponent implements OnInit{
   enteredTask = "";
   welcomeMessage: any;
   isEventsCritical = false;
-  beginApplication = true;
+  beginApplication = false;
   georgeEHarrisLogin = 528528;
   addStudentOn = false;
   criticalActivitiesOn = false;
@@ -100,7 +100,7 @@ export class AppComponent implements OnInit{
   displayStudentIcons = true;
   displayTaskIcons = true;
   displayTasksAll = true;
-  studentList: Student[] = [this.Michael, this.Andrea, this.Ash]
+  studentList: Student[] = []
   
   activeUser: User;
   dummyUser = new User(99999, "Foo", "[]");
